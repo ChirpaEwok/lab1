9e789b5dcb3a6482996fcf583ca239b72549c45d
ChirpaEwok <99390763+ChirpaEwok@users.noreply.github.com>
2022-03-24 20:35:09 +0300

    Update roots.c

--- app/roots.c
+++ app/roots.c
@@ -3,23 +3,20 @@
 
 #include "roots.h"
 
-double x1;
-double x2;
-
-int square_root(double a, double b, double c)
+int square_root(double a, double b, double c,  double *x1, double *x2)
 {
     if ((a == 0) && (b == 0)) return 0;
     double D = b * b - 4 * a * c;
     if (D < 0) return 0;
     if (D == 0)
     {
-	x1 = (-b) / (2 * a);
+	*x1 = (-b) / (2 * a);
 	return 1;
     }
     if (D > 0)
     {
-	x1 = ((-b) + sqrt(D)) / (2 * a);
-	x2 = ((-b) - sqrt(D)) / (2 * a);
+	*x1 = ((-b) + sqrt(D)) / (2 * a);
+	*x2 = ((-b) - sqrt(D)) / (2 * a);
 	return 2;
     }
     return 0;
