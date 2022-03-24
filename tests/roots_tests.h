9849f64445044df9c6fb2512f11adcaebe290c77
ChirpaEwok <99390763+ChirpaEwok@users.noreply.github.com>
2022-03-25 00:33:30 +0300

    Update roots_tests.h

--- tests/roots_tests.h
+++ tests/roots_tests.h
@@ -13,13 +13,19 @@ TEST(test1, zero_root) {
     ASSERT_EQ(0, square_root(2, 1, 2, &x1, &x2));
 }
 
-TEST(test1, one_root) {
+TEST(test2, one_root) {
     double x1 = 0;
     double x2 = 0;
     ASSERT_EQ(1, square_root(1, 2, 1, &x1, &x2));
 }
 
-TEST(test1, two_root) {
+TEST(test3, two_root) {
+    double x1 = 0;
+    double x2 = 0;
+    ASSERT_EQ(2, square_root(1, 6, 5, &x1, &x2));
+}
+
+TEST(test4, two_root) {
     double x1 = 0;
     double x2 = 0;
     ASSERT_EQ(2, square_root(1, 6, 5, &x1, &x2));
