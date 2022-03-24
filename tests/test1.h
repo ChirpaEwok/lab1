83399192f79a3307c12aa21fb928e451a3554d37
ChirpaEwok <99390763+ChirpaEwok@users.noreply.github.com>
2022-03-24 21:02:39 +0300

    Update test1.h

--- tests/test1.h
+++ tests/test1.h
@@ -14,10 +14,14 @@ TEST(test1, zero_root) {
 }
 
 TEST(test1, one_root) {
+    double x1 = 0;
+    double x2 = 0;
     ASSERT_EQ(1, square_root(1, 2, 1, &x1, &x2));
 }
 
 TEST(test1, two_root) {
+    double x1 = 0;
+    double x2 = 0;
     ASSERT_EQ(2, square_root(1, 6, 5, &x1, &x2));
 }
 
