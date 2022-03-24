0b82f49369afce776e135c50ce99f827df8110f3
ChirpaEwok <99390763+ChirpaEwok@users.noreply.github.com>
2022-03-24 20:38:28 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -12,7 +12,7 @@ int main()
     printf("Введите значения a,b и c: ");
     scanf("%lf%lf%lf", &a, &b, &c);
     int r = square_root(a, b, c, &x1, &x2);
-    if (r == 0) printf("Нет корней");
+    if (r == 0) printf("Дискриминант меньше 0, нет корней");
     if (r == 1) printf("Дискриминант равен 0, корень: %.2lf \n", x1);
     if (r == 2) printf("Дискриминант больше 0, корни: %.2lf, %.2lf \n", x1, x2);
     return 0;
