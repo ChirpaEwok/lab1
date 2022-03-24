cfaea7cac9a76043091f478fa95cc660f2dbe80b
ChirpaEwok <99390763+ChirpaEwok@users.noreply.github.com>
2022-03-24 20:34:01 +0300

    Update roots.h

--- app/roots.h
+++ app/roots.h
@@ -1,8 +1,6 @@
 #ifndef ROOTS_H
 #define ROOTS_H
 
-int square_root(double a,double b,double c);
-extern double x1;
-extern double x2;
+int square_root(double a,double b,double c, double *x1, double *x2);
 
 #endif // ROOTS_H
