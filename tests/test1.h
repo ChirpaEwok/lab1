c232fcecb9d44ed00ded1d721e68060ad20ce844
ChirpaEwok <99390763+ChirpaEwok@users.noreply.github.com>
2022-03-24 20:50:45 +0300

    Update test1.h

--- tests/test1.h
+++ tests/test1.h
@@ -1,18 +1,22 @@
+#ifndef TEST_H
+#define TEST_H
+
 #include <gtest/gtest.h>
-#include <math.h>
 
 extern "C" {
 #include "roots.h"
 }    
 TEST(test1, zero_root) {
-    ASSERT_EQ(0, square_root(0, 0, 5));
-    ASSERT_EQ(0, square_root(2, 1, 2));
+    ASSERT_EQ(0, square_root(0, 0, 5, &x1, &x2));
+    ASSERT_EQ(0, square_root(2, 1, 2, &x1, &x2));
 }
 
 TEST(test1, one_root) {
-    ASSERT_EQ(1, square_root(1, 2, 1));
+    ASSERT_EQ(1, square_root(1, 2, 1, &x1, &x));
 }
 
 TEST(test1, two_root) {
-    ASSERT_EQ(2, square_root(1, 6, 5));
+    ASSERT_EQ(2, square_root(1, 6, 5, &x1, &x));
 }
+
+#endif // TEST_H
