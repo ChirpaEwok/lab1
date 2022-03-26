a3661111d12bd62b0713ef56796cbfc9398449f9
ChirpaEwok <99390763+ChirpaEwok@users.noreply.github.com>
2022-03-26 16:05:07 +0300

    Update roots_tests.h

--- tests/roots_tests.h
+++ tests/roots_tests.h
@@ -3,7 +3,7 @@
 extern "C" {
 #include "roots.h"
 }    
-TEST(test1, zero_root) {
+TEST(amount_of_roots, zero_root) {
     double x1 = 0;
     double x2 = 0;
     ASSERT_EQ(0, square_root(0, 0, 5, &x1, &x2));
@@ -11,16 +11,26 @@ TEST(test1, zero_root) {
     ASSERT_EQ(0, square_root(1.5, 0.5, 1.5, &x1, &x2));
 }
 
-TEST(test2, one_root) {
+TEST(amount_of_roots, one_root) {
     double x1 = 0;
     double x2 = 0;
     ASSERT_EQ(1, square_root(1, 2, 1, &x1, &x2));
     ASSERT_EQ(1, square_root(0.8, 1.6, 0.8, &x1, &x2));
 }
 
-TEST(test3, two_root) {
+TEST(amount_of_roots, two_root) {
     double x1 = 0;
     double x2 = 0;
     ASSERT_EQ(2, square_root(1, 6, 5, &x1, &x2));
     ASSERT_EQ(2, square_root(1.5, 5, 1.5, &x1, &x2));
+}
+
+TEST(root_values, one_root) {
+    double x1 = 0;
+    double x2 = 0;
+    EXPECT_EQ(1, square_root(1, 2, 1, &x1, &x2));
+    if (x1 == -1)
+        SUCCEED();
+    else
+        FAIL();
 }
