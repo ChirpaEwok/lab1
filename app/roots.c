505773d6c4f99c9501fe22bb359def9590722f11
Даниил Сергин <99390763+ChirpaEwok@users.noreply.github.com>
2022-04-14 10:44:45 +0300

    Update roots.c

--- app/roots.c
+++ app/roots.c
@@ -28,11 +28,8 @@ int square_root(double a, double b, double c,  double *x1, double *x2)
 	*x1 = (-b) / (2 * a);
 	return 1;
     }
-    if (D > 0)
-    {
-	*x1 = ((-b) + sqrt(D)) / (2 * a);
-	*x2 = ((-b) - sqrt(D)) / (2 * a);
-	return 2;
-    }
-    return 0;
+	
+    *x1 = ((-b) + sqrt(D)) / (2 * a);
+    *x2 = ((-b) - sqrt(D)) / (2 * a);
+    return 2;
 }
